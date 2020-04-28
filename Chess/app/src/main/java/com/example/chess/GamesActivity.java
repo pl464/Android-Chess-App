96be3079445010928260a78cbb1925f81fc7401b
unknown <lance.luo@rutgers.edu>
2020-04-27 21:43:15 -0400

    added activity navigation

--- Chess/app/src/main/java/com/example/chess/GamesActivity.java
+++ Chess/app/src/main/java/com/example/chess/GamesActivity.java
@@ -2,7 +2,9 @@ package com.example.chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GamesActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class GamesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
+
+        Intent intent = getIntent();
+    }
+
+    public void dateSort(View v) {
+        //todo
+    }
+
+    public void titleSort(View v) {
+        //todo
+    }
+
+    public void showPlayback(View v) {
+        startActivity(new Intent(this, PlaybackActivity.class));
     }
 }
--- Chess/app/src/main/java/com/example/chess/HomeActivity.java
+++ Chess/app/src/main/java/com/example/chess/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void showMain(View v) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    public void showGames(View v) {
+        startActivity(new Intent(this, GamesActivity.class));
     }
 }
