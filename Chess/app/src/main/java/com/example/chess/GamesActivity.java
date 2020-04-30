4751c0c44a1ab3c29dff5c8680655a8c21163e45
unknown <lance.luo@rutgers.edu>
2020-04-30 15:56:11 -0400

    display saved games and sort by title/date

--- Chess/app/src/main/java/com/example/chess/GamesActivity.java
+++ Chess/app/src/main/java/com/example/chess/GamesActivity.java
@@ -5,23 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class GamesActivity extends AppCompatActivity {
+
+    private ListView gameList;
+    private ArrayAdapter adapter;
+    private File[] files;
+    private String[] names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games);
 
-        Intent intent = getIntent();
+        File dir = this.getFilesDir();
+        files = dir.listFiles();
+        names = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            names[i] = files[i].getName();
+        }
+
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, names);
+        gameList = findViewById(R.id.gameList);
+        gameList.setAdapter(adapter);
     }
 
     public void dateSort(View v) {
-        //todo
+        Arrays.sort(files, (a, b) -> Long.compare(a.lastModified(), b.lastModified()));
+        for (int i = 0; i < files.length; i++) {
+            names[i] = files[i].getName();
+        }
+        adapter.notifyDataSetChanged();
     }
 
     public void titleSort(View v) {
-        //todo
+        Arrays.sort(names);
+        adapter.notifyDataSetChanged();
     }
 
     public void showPlayback(View v) {
